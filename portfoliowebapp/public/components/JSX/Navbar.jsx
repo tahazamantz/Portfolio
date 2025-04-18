1723d67b453ac397e6fe968ad4da9ab92e02f760
Taha Zaman <tz.tahazaman@gmail.com>
2025-04-18 15:37:41 +0500

    C4

--- portfoliowebapp/public/components/JSX/Navbar.jsx
+++ portfoliowebapp/public/components/JSX/Navbar.jsx
@@ -2,29 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import '../CSS/Navbar.css'
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="fixed-top shadow-lg p-3 mb-5 bg-body rounded bg-transparent">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+            <Nav.Link href="#link">About Me</Nav.Link>
+            <Nav.Link href="#home">Skills</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#home">Public Message Board</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+
+         </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
