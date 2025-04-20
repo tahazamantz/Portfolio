55b7b889fa72811be73ea1f3dcf15e1b8b3c5a84
Taha Zaman <tz.tahazaman@gmail.com>
2025-04-21 01:10:24 +0500

    C5

---
+++ portfoliowebapp/public/components/JSX/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../CSS/Home.css'
+function Home() {
+  return (
+    <div className='main'>
+               <h5>Hello! I'm Taha Zaman.</h5>
+               <br />
+               <h1>Making life easier by some lines of <mark> Code </mark>. </h1>
+               <h5>I transform diminutive ideas into groundbreaking product by  inventing greater UI & UX through clean code and intuitive designs </h5>
+               </div>
+  )
+}
+
+export default Home
--- portfoliowebapp/public/components/JSX/Navbar.jsx
+++ portfoliowebapp/public/components/JSX/Navbar.jsx
@@ -3,11 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../CSS/Navbar.css'
+import logo1 from '../images/logo1.jpg'
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="fixed-top shadow-lg p-3 mb-5 bg-body rounded bg-transparent">
+    <Navbar expand="lg" className="fixed-top shadow-lg p-1 mb-5 bg-body rounded bg-transparent">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo1}/>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
