92096c23af1991e0808318568611ae46334ee980
Taha Zaman <tz.tahazaman@gmail.com>
2025-04-21 01:35:44 +0500

    C6

--- portfoliowebapp/src/App.jsx
+++ portfoliowebapp/src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 import Navbar from '../public/components/JSX/Navbar.jsx'
 import Home from '../public/components/JSX/Home.jsx'
 function App() {
- 
+  const toggleTheme = () => {
+    const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
+    document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+    localStorage.setItem('theme', isDark ? 'light' : 'dark');
+  };
   return (
     <>
     <div className='main'>
