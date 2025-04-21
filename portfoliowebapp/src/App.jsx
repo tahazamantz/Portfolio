975c433aa457664512022e3bebb659ffe5eb8a02
Taha Zaman <tz.tahazaman@gmail.com>
2025-04-21 11:44:56 +0500

    C6

--- portfoliowebapp/src/App.jsx
+++ portfoliowebapp/src/App.jsx
@@ -17,8 +17,12 @@ function App() {
     </nav>
      <div>
       <Home/>
+      <br />
+      <Home/>
+      <Home/>
+      <Home/>
+
      </div>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis sed cum necessitatibus aspernatur qui. Necessitatibus eos veritatis, dolor eligendi exercitationem eum omnis obcaecati ratione ipsam qui distinctio deserunt numquam impedit quidem dolore voluptatum. Deserunt, veritatis delectus? Libero ipsa obcaecati culpa, sapiente facere tempore laborum nesciunt. Vel dicta cumque, placeat id dolor deleniti reiciendis itaque dolores perspiciatis ducimus sit aut unde vero nemo libero quam neque eos tenetur, natus quos corporis ut delectus consequatur exercitationem. Fugit eveniet dicta non deserunt ea, nihil dolor placeat commodi earum ad ducimus. Nostrum quam a architecto omnis. Ab, maiores in nam voluptates reprehenderit laborum sit!</p>
     </div> 
     </>
   )
